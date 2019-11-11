3f9b025731fb8f167f99b43af9bc5d457ef9d813
Jörn Röder <kontakt@joernroeder.de>
2019-11-11 15:36:19 +0100

    added useReadableFileSize hook

---
+++ src/hooks/format/useReadableFileSize.js
@@ -0,0 +1,27 @@
+import { useMemo } from 'react'
+
+/**
+ * converts the given size in bytes into a readable format
+ *
+ * @see https://stackoverflow.com/a/20732091
+ *
+ * @param {number} sizeInBytes
+ * @returns {string}
+ */
+const useReadableFileSize = (sizeInBytes) => {
+  return useMemo(() => {
+    if (!sizeInBytes) {
+      return '0b'
+    }
+
+    const i = Math.floor(Math.log(sizeInBytes) / Math.log(1024))
+
+    return (
+      (sizeInBytes / Math.pow(1024, i)).toFixed(2) * 1 +
+      //' ' +
+      ['b', 'kb', 'mb', 'gb', 'tb'][i]
+    )
+  }, [sizeInBytes])
+}
+
+export default useReadableFileSize
