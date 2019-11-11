78c361cecbd0bfa26fb1bac396cf336545dc45be
Jörn Röder <kontakt@joernroeder.de>
2019-11-11 12:24:52 +0100

    standardized code format
    - updated linting via eslint
    - added formatting via prettier
    - auto format and lint via pre-commit hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './tailwind.build.css';
+import React from 'react'
+import './tailwind.build.css'
 
 function App() {
   return (
     <div>
       <h1 className="bg-black text-white">Testing</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
