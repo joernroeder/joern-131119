69648293d2688a07653d88450a3e85f46b40189e
Jörn Röder <kontakt@joernroeder.de>
2019-11-11 12:45:37 +0100

    updated test setup
    - added testing-library
    - added eslint-plugin
    - updated initial test
    - added coverage script
    - added html coverage reports

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+test('renders without crashing', () => {
+  const { queryByText, container } = render(<App />)
+
+  expect(queryByText('Testing')).toBeInTheDocument()
+  expect(container).toMatchSnapshot()
 })
