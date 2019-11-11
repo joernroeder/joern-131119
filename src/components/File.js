5d92eb1a3309dc585a8b772e9d4a7dec739afe52
Jörn Röder <kontakt@joernroeder.de>
2019-11-11 15:38:30 +0100

    updated file component and tests

--- src/components/File.js
+++ src/components/File.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import useReadableFileSize from '../hooks/format/useReadableFileSize'
+
 const File = ({ id, name, size }) => {
+  const readableSize = useReadableFileSize(size)
+
+  if (!id) {
+    return null
+  }
+
   return (
     <article className="w-full sm:w-1/2 md:w-1/3 px-2 pb-4">
       <div className="border p-4 h-full">
-        <h2 className="text-2xl mb-2">Doc1</h2>
+        <h2 className="text-2xl mb-2">{name}</h2>
         <div className="flex justify-between items-baseline">
-          <div className="mr-4">100kb</div>
+          <div className="mr-4">{readableSize}</div>
           <div>
             <button
               type="button"
--- src/components/File.test.js
+++ src/components/File.test.js
@@ -4,6 +4,30 @@ import { render } from '@testing-library/react'
 
 import File from './File'
 
+test('files without/with invalid id should be treated as invalid', () => {
+  const props = {
+    id: '',
+    name: 'my-file-name.txt',
+    size: 42,
+  }
+
+  const { container } = render(<File {...props} />)
+
+  expect(container).toBeEmpty()
+})
+
+test('it should correctly show the file name', () => {
+  const props = {
+    id: 'someId',
+    name: 'my-file-name.txt',
+    size: 0,
+  }
+
+  const { queryByText } = render(<File {...props} />)
+
+  expect(queryByText('my-file-name.txt')).toBeInTheDocument()
+})
+
 test('delete button should exist', () => {
   const props = {
     id: 'someId',
