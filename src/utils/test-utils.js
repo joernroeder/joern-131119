33ca2b27dcfc6da30a5efd412357490e49210cbd
Jörn Röder <kontakt@joernroeder.de>
2019-11-13 03:46:27 +0100

    added jest-mock-axios module to improve request testing

---
+++ src/utils/test-utils.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+
+import { FilesProvider } from '../store/FileStore'
+import { FilteredFilesProvider } from '../store/FilteredFileStore'
+import { ApiProvider } from '../api/ApiContext'
+
+const WithFilesProvider = ({ children }) => {
+  return (
+    <FilesProvider>
+      <FilteredFilesProvider>{children}</FilteredFilesProvider>
+    </FilesProvider>
+  )
+}
+
+const WithApiAndFilesProvider = ({ children }) => {
+  return (
+    <ApiProvider>
+      <WithFilesProvider>{children}</WithFilesProvider>
+    </ApiProvider>
+  )
+}
+
+const renderWithFilesProviders = (ui, options) => {
+  return render(ui, { wrapper: WithFilesProvider, ...options })
+}
+
+const renderWithApiAndFileProviders = (ui, options) => {
+  return render(ui, { wrapper: WithApiAndFilesProvider, ...options })
+}
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { renderWithFilesProviders, renderWithApiAndFileProviders }
