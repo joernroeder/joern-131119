ae443822502ea7c1759bdb517072e2f3dce2438a
Jörn Röder <kontakt@joernroeder.de>
2019-11-13 03:28:13 +0100

    updated File component and tests

--- src/components/File.js
+++ src/components/File.js
@@ -1,24 +1,74 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import PropTypes from 'prop-types'
+
 import useReadableFileSize from '../hooks/format/useReadableFileSize'
+import { useApiContext, ApiStatus } from '../api/ApiContext'
+import { useFilesDispatch, Actions } from '../store/FileStore'
 
 const File = ({ id, name, size }) => {
+  const [isDeleting, setIsDeleting] = useState(false)
+  const dispatch = useFilesDispatch()
   const readableSize = useReadableFileSize(size)
+
+  const api = useApiContext()
+  const [status, setStatus] = useState(undefined)
+  const [cancelToken, _] = useState(api.getCancelToken())
+  let [isCanceled, setIsCanceled] = useState(false)
+
+  useEffect(() => {
+    return () => {
+      cancelToken.cancel()
+      setIsCanceled(true)
+    }
+  }, [cancelToken])
+
+  const onDeleteClick = useCallback(
+    (event) => {
+      event.preventDefault()
+
+      setIsDeleting(true)
+      setStatus(ApiStatus.LOADING)
+
+      api
+        .deleteFile({ withId: id, cancelVia: cancelToken })
+        .then((file) => {
+          if (isCanceled) {
+            return
+          }
+          console.log(file)
+
+          dispatch({ type: Actions.DELETE_FILE, payload: { id } })
+          setStatus(ApiStatus.SUCCESS)
+        })
+        .catch(() => {
+          if (isCanceled) {
+            return
+          }
+          setStatus(ApiStatus.ERROR)
+        })
+    },
+    [api, cancelToken, isCanceled, dispatch, id]
+  )
 
   if (!id) {
     return null
   }
 
   return (
-    <article className="w-full sm:w-1/2 md:w-1/3 px-2 pb-4">
+    <article
+      className="w-full sm:w-1/2 md:w-1/3 px-2 pb-4"
+      style={{ opacity: isDeleting ? 0.5 : 1 }}
+    >
       <div className="border p-4 h-full">
-        <h2 className="text-2xl mb-2">{name}</h2>
+        <h2 className="text-2xl mb-2 overflow-hidden">{name}</h2>
         <div className="flex justify-between items-baseline">
           <div className="mr-4">{readableSize}</div>
           <div>
             <button
               type="button"
-              className="text-sm border border-blue-900 bg-blue-400 hover:bg-blue-900 hover:text-white focus:bg-blue-900 focus:text-white py-1 px-4"
+              className="text-sm border border-blue-900 bg-blue-400 hover:bg-blue-900 hover:text-white focus:bg-blue-900 focus:text-white disabled:text-gray-900 disabled:bg-gray-400 disabled:cursor-default py-1 px-4"
+              onClick={onDeleteClick}
+              disabled={isDeleting}
             >
               delete
             </button>
--- src/components/File.test.js
+++ src/components/File.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { renderWithApiAndFileProviders, fireEvent, act, wait } from 'test-utils'
 
 import File from './File'
+import mockAxios from 'jest-mock-axios'
 
 test('files without/with invalid id should be treated as invalid', () => {
   const props = {
@@ -11,7 +11,7 @@ test('files without/with invalid id should be treated as invalid', () => {
     size: 42,
   }
 
-  const { container } = render(<File {...props} />)
+  const { container } = renderWithApiAndFileProviders(<File {...props} />)
 
   expect(container).toBeEmpty()
 })
@@ -23,9 +23,21 @@ test('it should correctly show the file name', () => {
     size: 0,
   }
 
-  const { queryByText } = render(<File {...props} />)
+  const { queryByText } = renderWithApiAndFileProviders(<File {...props} />)
 
   expect(queryByText('my-file-name.txt')).toBeInTheDocument()
+})
+
+test('it should correctly escape', () => {
+  const props = {
+    id: 'someId',
+    name: '<p>my-file-name.txt</p>',
+    size: 0,
+  }
+
+  const { queryByText } = renderWithApiAndFileProviders(<File {...props} />)
+
+  expect(queryByText('<p>my-file-name.txt</p>')).toBeInTheDocument()
 })
 
 test('delete button should exist', () => {
@@ -35,7 +47,32 @@ test('delete button should exist', () => {
     size: 0,
   }
 
-  const { queryByText } = render(<File {...props} />)
+  const { queryByText } = renderWithApiAndFileProviders(<File {...props} />)
 
   expect(queryByText('delete')).toBeInTheDocument()
+})
+
+test('hitting the delete button should correctly send the api request', async () => {
+  const props = {
+    id: "someId?='id!=0",
+    name: 'fileName',
+    size: 42,
+  }
+
+  const { queryByText } = renderWithApiAndFileProviders(<File {...props} />)
+
+  const button = queryByText('delete')
+
+  act(() => {
+    fireEvent.click(button)
+  })
+
+  await wait(() => {
+    expect(mockAxios.delete).toHaveBeenCalled()
+    expect(mockAxios.delete.mock.calls[0][0]).toBe(
+      "/files/someId%3F%3D'id!%3D0"
+    )
+
+    mockAxios.mockResponse({ status: 200, statusText: 'OK' })
+  })
 })
