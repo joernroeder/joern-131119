f6fa868541695c429ce87a5f7f810df92cf10ef6
Jörn Röder <kontakt@joernroeder.de>
2019-11-13 03:51:22 +0100

    added container plugin with specific max-width values to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,25 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    function({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '600px',
+          },
+          '@screen md': {
+            maxWidth: '700px',
+          },
+          '@screen lg': {
+            maxWidth: '800px',
+          },
+          '@screen xl': {
+            maxWidth: '960px',
+          },
+        },
+      })
+    },
+  ],
 }
