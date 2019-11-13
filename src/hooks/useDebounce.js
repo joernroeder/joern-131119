d747f2064989561b3993d31d9743d4b07e0eb53c
Jörn Röder <kontakt@joernroeder.de>
2019-11-13 01:45:44 +0100

    added useDebounce hook

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [delay, value])
+
+  return debouncedValue
+}
+
+export default useDebounce
